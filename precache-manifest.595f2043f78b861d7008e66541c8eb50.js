bbef8887e36464f9220634e827f07301094b0f60
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2019-08-03 12:38:46 -0500

    Updates

---
+++ precache-manifest.595f2043f78b861d7008e66541c8eb50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e56b48aea05a6f35d9b04c8581b78ae",
+    "url": "/TalentFestGDL002/index.html"
+  },
+  {
+    "revision": "5640603f4aeebc56d656",
+    "url": "/TalentFestGDL002/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/TalentFestGDL002/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5640603f4aeebc56d656",
+    "url": "/TalentFestGDL002/static/js/main.15696266.chunk.js"
+  },
+  {
+    "revision": "088a6520dd0bd48cb879",
+    "url": "/TalentFestGDL002/static/js/runtime~main.8dca6983.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TalentFestGDL002/static/media/logo.5d5d9eef.svg"
+  }
+]);
