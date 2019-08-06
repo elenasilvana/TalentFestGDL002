17aa615fdd52d4c1cf77e7cdb63d16a89b72c4ac
lumenbarron <lucemendez@gmail.com>
2019-08-06 16:28:42 -0500

    changing images

--- src/Data/QuizData.js
+++ src/Data/QuizData.js
@@ -1,34 +1,34 @@
 export const QuizData = [
     {
         id: 1,
-        image : 'https://lh3.googleusercontent.com/yDKPDtSRN-YQArZRi4Tu9ehRJWJoSCqWV7U3kPAArADQcSp4o3Y5jrJEg2o38DNTk6LzInu3j2vNs8l3tXzLM2CoEy_IaCFRM9O0PKh6HjE_YnP45HoiB5XYgrqDQGU1zHxKvM-tW0kP5d_BzzHsAbDFpZVzxsHLdBXXZh3rtwxAtCc_6BjI6u7ixRELWD5zIBeBCrEjcue_DfGZ2zaAYltLu7OjGaFhflyg_i5etVmzzE709IdhkluXEvjq4fRowP8dy-VUqTjoIXdqCxVeiL-0JbjmURDVLbNwB-lIaApP_XbOdiEF3KZwcYg6mK2JWLQB--v2C1a8nMUtqcykk56-1BSxM950kUvQKfqtePIQ3nOfGIQZmkLPSyw2-Y3VbMZu_1FiXNTd--tQSDHR-eHueLLiFkSd-rgOecLzXNwlraV66J7_dNcOVLzC2Gw765DMf0g_yshsgZpxrtoTKmRTLagP3HssUETnXLY9Vig1D6gdf1PfCrexq4VIeZryqMc9aNb6MBao9-L4HMtLXGuBGQ9o34khabInUO4l31jxtCifXij1hshPZnKTWNnfGRMKYqarN3moZ0fdUBRxs9fU-ueV_Jta5sxOFxxDrOzqjg9HjblwEjUwSanpUklQVmyBx3dldEN-uLjPWldFVgcQQ0x1HdY=s151-no',
+        image : 'https://i.postimg.cc/g0LHGzGc/a-1.png',
         question: '¿Qué letra representa la seña?',
         options: ['A', 'Y', 'E'],
         answer: 'A'
     },
     {
         id: 2,
-        image:'https://lh3.googleusercontent.com/_GXqGVgCoThLVptvZ-aLj2oTXU0OEjKnnG5getafdGmXv4X3w3RRYmg-YRYxyABatoHUgwhBKF_6BzUO_2-Yw0GW7wZ67Z0klXCYMwkbp4_geQ5pmjsCNhzq_lTOH3t4CfiMoYBlYYUP78Fcdbf-6E-HFMNlYJsWHhgnf4yjTAkpsBHdnr9l7bAJy-_M1nHgRTbtZmWYveapC42t5rdPZ7S5XSCpz5Jt9mzePzoFeqaABXOK78Ep6JvK6cAGjW4Jdyn5ci5oBejuf56BNX79-IcSIcztd9_aXNl13uSdABqGLPXSR9TkV8Q1aXwGtfa41na7FVvz6Wj49UXKyLeGAzvsB8qKA7tcK63HwXRhSEkCzDjzINAwm8yhE5J3qKRN0HNObX0feFKvQiRHaBMLuGroX0zQFJ6-0YAMgyzo1134NmIMQzW9eacLCVIPc6kogfjiMfbleZ0b_LkJCvZpLRWpgtr2CTVfl-ksIUVK5YyHOnWW5E3CMd4zbdKqBGrphDTy-6phoiKkP9B8BpZaWMEjReoiw9HVoi68s7QymRZoOq34UWNQqqF45WEgFWim_uOshPmoordFfmJVNeNcgRDhoVFpUgFvqXVLgnAIKcFjE7mluyKLSQeYS7OjRR_C-6BSavRn83_2hSxzECyFk_T3JHP0TkQ=s151-no',
+        image:'https://i.postimg.cc/rmSGtfdv/k-1.png',
         question: '¿Qué letra representa la seña?',
         options: ['Q', 'K', 'E'],
         answer: 'K'
     },
     {
         id: 3,
-        image:'https://lh3.googleusercontent.com/UE1RpC8qm82VMe8c_MnbVklv6oDr8IeT_g8bfaDIiqvUKbBBithl2nuMPhEbbyUBhl3pQWZM-eU5yyF1YhXFHiZ3JVoW3SBwJWVpDIdB7EQ59LdOwH2miuKEFlufeQmdchZG-etyx8EbMCwIad6CuDnXFb4OAnUr0PO8ZjQOE8WI9ZjpulTkO3giH1e6qXLYnQENf_aX_pB85ODpcaX4UcULdPn3MKTuHByyLVl7ab3c4KADpTraeJoP1rwB038FIPjIcxLscY9OaOLIYe05Pd2YTE9xh1t54UxGo4WEoIKOF1vnHtn7ccTio4VCbe8VKKYs5ZEEyYfTJBKc4X_e-gmdaDEMc60S4Bz_yEEcaSqIA-K1rU0Y0jUzq-o8nAQZr6ypOXF4VU__X82IsCSS1nuFnO7gqmtAIqbWxzVRGXTxefroFdrYUw-tKkBldc2d9kks-R9fwtb8VxeLFEtar-3_AC5JV1s2SBHtsQNnVGAGpg4pvzW-nkrwOOG1J5nD6sH6kuv1Gf4VJva0Y6w_O3iqMSzKlemfsIKHpUjF6c7XLikp6z0-Xx66yr4vHZzMgk7J6T9wqglzFmPn5KkzxbC9-Z0kNH0KXyBcx-EbBAu9APMEbaEBHEsLzWl69zCNougmoqsz2lXthrQnmZymsz9HkNUVmHM=s151-no',
+        image:'https://i.postimg.cc/B6Wc6rvT/l-1.png',
         question: '¿Qué letra representa la seña?',
         options: ['L', 'M', 'R'],
         answer: 'L'
     },
     {
         id: 4,
-        image:'https://lh3.googleusercontent.com/gMsd_vYaNtBwIR78zZfOAwWHl2M9wvIh43FRWClKLm7-p4mhVeT9y4pUaaXjkVP1HBxDWXDUKdmEnMFkFHh4JYRzwTfwy5o-_LB7vSaP9QUYWGNsu7CINOlOwZsYx7odVDjyWxavrX3H8kNlNA4z1D0atcmVfmGFKRkq5CIDGDkhmcasW828bmW1nQ1UgXl7mCXSFLs_mhX5L65Ro1aXxkmC36N28Dq6hnB0y8IQ1V7DlH76XKMAo0yAtHld2nx3heOIGOKNpGLykoafDpetvzHEObt8WUHuWYbXUU8fI1Q84zRJwP3l7FQaetTmcZb0GQHMmCgJ2hAeFrfRU1VyK5BzlxptasQ97ok8HM57QgGnwAiADDhQHy8JFC52-pTbV6dl6kKdMXl3Bj9EmygXI_oMNoeimX5iknYsyMbGn_t_Lz6qAMxLctKP_N-d-PxkBUupcNliqcmCcjNEBPmqj5RKwUehe9PHCvVd6MXDB7d7PyWSkQUeVPaLzzZwGNPo3HZ7F_ZrRLYem_ouef559aDu_ntz0mCffaRPE-9G68AX39hgLI-SpPJccHe3PYz9hoTThpAu21OSbVOtvzbP5qWpCo8MZKG5e1PRnSukHyZVW9MCm4oJPNux_c8gcp9g02WmyXoBOZFRiMmbYPF3Qj1TR1x9aiI=s151-no',
+        image:'https://i.postimg.cc/yxsmh0cW/n-1.png',
         question: '¿Qué letra representa la seña?',
         options: ['O', 'W', 'N'],
         answer: 'N'
     },  {
         id: 5,
-        image:'https://lh3.googleusercontent.com/H7oeazns8sKdzpqzALxJOlUZNp6ZSbBqV93r4LC4D_Y4-EcN_8WjaJUF9xEuXjTHgiG4t44SQ9BNh9NHOkJoaD3NtlYtOgt0HXV573YWDENJhC_ez8o_6yR3fVK0J-EB_91I_ocLh3pYSPVFIROajYF_K0tbERqa0BJWkCuecbuyflT7fc6mYam4iI0LB0p8FhOU1wrR01qwJK4p9LvFc8uH6n5Ki1HsaqGUOKSc0BnVrYdDCwozl-zYB43ADywMMGpLuOVFlp86ut3D7dY5590651cpaGYCYM1EVGKVuZyr3PJ71KakhBbaPW7nHhvr1R636FiF9lLGfiYaAjkM96eFUT9bvNcvJkkBVAl0X9S3RkfXBUB3ElvO7M3KIqGhauUW7WxXYsD6rjdt4sx1rPOL5iKJl5KPlsIqR1PIkcleKB9vVsYL9bFHFmN-FThmdmKfNkRUAQ3ceQOMXskqDF699HC_XNUmjGYtbfYIyeZGeyWYKXC_YWc7OuRKt7UcM3M_tKBx5uzTbtHH-UfoAtcWF17NRIS8XYkeKg1Lfy-chrWezaOY8jahOrd0ZwJ1LfdgdE7ZPQI40bpHgFfMTQ7nN-mbsfd595XVcsI--P2zx6pAa_ktyrN0y5PP967QkKh3EgTpmXuPnKDrHpOJZmTbxIGeEeY=s151-no',
+        image:'https://i.postimg.cc/kGsxTDvs/y-1.png',
         question: '¿Qué letra representa la seña?',
         options: ['Z', 'Y', 'O'],
         answer: 'Y'
