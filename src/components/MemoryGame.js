30039107e8e2d602567db2454470f40d90b02b3b
lumenbarron <lucemendez@gmail.com>
2019-08-06 15:59:55 -0500

    adding trivia and making responsive

--- src/components/MemoryGame.js
+++ src/components/MemoryGame.js
@@ -1,148 +1,134 @@
 import React, { Component } from 'react';
-import ShowCard from './ShowCard';
-import * as CARDIMG from '../Data/IMAGES';
 import Nav from './NavBar';
-import a from '../ImagesTrivia/a-1.png';
-import k from '../ImagesTrivia/k-1.png';
-import n from '../ImagesTrivia/n-1.png';
-import l from '../ImagesTrivia/l-1.png';
-import y from '../ImagesTrivia/y-1.png';
 import './cards.css';
 import {QuizData} from '../Data/QuizData';
 
 class MemoryGame extends Component {
-    state = {
-      userAnswer: null,
-      currentQuestion: 0,
-      options: [],
-      quizEnd: false,
-      score: 0,
-      disabled:true
-    }
-  
+  state = {
+    userAnswer: null,
+    currentQuestion: 0,
+    options: [],
+    quizEnd: false,
+    score: 0,
+    disabled: true
+  };
+
   loadQuiz = () => {
     const { currentQuestion } = this.state;
     this.setState(() => {
       return {
         questions: QuizData[currentQuestion].question,
         options: QuizData[currentQuestion].options,
-        answer: QuizData[currentQuestion].answer
-      }
-    })
-  }
+        answer: QuizData[currentQuestion].answer,
+        images: QuizData[currentQuestion].image
+      };
+    });
+  };
   componentDidMount() {
     this.loadQuiz();
   }
   nextQuestionHandler = () => {
-    const {userAnswer,answer,score} = this.state
+    const { userAnswer, answer, score } = this.state;
     this.setState({
       currentQuestion: this.state.currentQuestion + 1
-    })
-    if (userAnswer===answer)
-    { console.log('correcta')
-      this.setState({score:score+1})}
-      else{
-        console.log('incorrecta')
-      }
+    });
+    if (userAnswer === answer) {
+      let correct = "correcta";
+      console.log("correcta");
+      this.setState({ score: score + 1 });
+    } else {
+      console.log("incorrecta");
+    }
     //console.log(this.state.currentQuestion);
-  }
+  };
 
-  componentDidUpdate(prevProps, prevState){
-    const {currentQuestion} = this.state;
-    if(this.state.currentQuestion !== prevState.currentQuestion){
-      this.setState(()=>{
+  componentDidUpdate(prevProps, prevState) {
+    const { currentQuestion } = this.state;
+    if (this.state.currentQuestion !== prevState.currentQuestion) {
+      this.setState(() => {
         return {
           disabled: true,
           questions: QuizData[currentQuestion].question,
           options: QuizData[currentQuestion].options,
-          answer: QuizData[currentQuestion].answer
-        }
-      })
+          answer: QuizData[currentQuestion].answer,
+          images: QuizData[currentQuestion].image
+        };
+      });
     }
   }
-  
-checkAnswer = (answer) => {
-  this.setState({userAnswer:answer,disabled:false})
 
-}
+  checkAnswer = answer => {
+    this.setState({ userAnswer: answer, disabled: false });
+  };
 
-finishHandler = () => {
-  if(this.state.currentQuestion === QuizData.length -1) {this.setState({quizEnd :true})}
-}
+  finishHandler = () => {
+    if (this.state.currentQuestion === QuizData.length - 1) {
+      this.setState({ quizEnd: true });
+    }
+  };
 
   render() {
-    const { questions, options, currentQuestion, userAnswer, quizEnd } = this.state
-    if(quizEnd){
-      return(
+    const {
+      questions,
+      options,
+      currentQuestion,
+      userAnswer,
+      quizEnd,
+      images
+    } = this.state;
+    if (quizEnd) {
+      return (
         <div>
-          <h3>Terminaste, tu resultado final es de {this.state.score} aciertos</h3>
-          {}
+          <Nav />
+          <div className="final-score">
+          <p className="score-title">
+            Terminaste, tu resultado final es de {this.state.score} aciertos
+          </p>
+          </div>
+
         </div>
-      )
+      );
     }
     return (
       <div>
         <Nav />
         <section className="container-lsm">
           <h5 className="title-lsm">Trivia</h5>
-          <div>
-            <h4>{questions}</h4>
-            <span>{`pregunta ${currentQuestion} de ${QuizData.length - 1}`}</span>
-            {options.map(option=>(
-              <p className={`text-select ${userAnswer === option ? 'selected':null}`} onClick={ () => this.checkAnswer(option)} key={option.id} > 
+          <div className="question-container">
+            <p className="score-counter">{`pregunta ${currentQuestion} de ${QuizData.length -
+              1}`}</p>
+            <p className="score-title">{questions}</p>
+
+            <img className="image-trivia" src={images} />
+            {options.map(option => (
+              <p
+                className={` select-text info-sordo text-select ${
+                  userAnswer === option ? "selected" : null
+                }`}
+                onClick={() => this.checkAnswer(option)}
+                key={option.id}
+              >
                 {option}
               </p>
-
             ))}
-            {currentQuestion < QuizData.length - 1 && <button disabled={this.state.disabled}
-            onClick={this.nextQuestionHandler}>
-              siguiente
+            <p id="correct">{}</p>
+            {currentQuestion < QuizData.length - 1 && (
+              <button
+              className="waves-effect waves-light btn btn-next"
+                disabled={this.state.disabled}
+                onClick={this.nextQuestionHandler}
+              >
+                siguiente
+              </button>
+            )}
 
-            </button>}
-            
-            {currentQuestion === QuizData.length -1 && <button onClick={this.finishHandler}>Terminar
-            </button>}
+            {currentQuestion === QuizData.length - 1 && (
+              <button
+              className="waves-effect waves-light btn btn-next"
+               onClick={this.finishHandler}>Terminar</button>
+            )}
           </div>
-          <section className="info-container">
-            <img className="image-sordo" src={a} />
-            <section className="info-ver-mas">
-              <p className="flow-text info-sordo">
-                ¿Qué letra representa la seña?
-              </p>
-
-              <select>
-                <option value="" disabled selected>
-                  Selecciona la opción correcta
-                </option>
-                <option value="1">A</option>
-                <option value="2">E</option>
-                <option value="3">J</option>
-              </select>
-            </section>
-
-            {/* <button className="waves-effect waves-light btn btn-siguiente" >Siguiente</button> */}
-          </section>
-
-          <section className="info-container">
-            <img className="image-sordo" src={n} />
-            <section className="info-ver-mas">
-              <p className="flow-text info-sordo">
-                ¿Qué letra representa la seña?
-              </p>
-
-              <select>
-                <option value="" disabled selected>
-                  Selecciona la opción correcta
-                </option>
-                <option value="1">A</option>
-                <option value="2">E</option>
-                <option value="3">J</option>
-              </select>
-            </section>
-
-            {/* <button className="waves-effect waves-light btn btn-siguiente" >Siguiente</button> */}
-          </section>
-
+          <section />
         </section>
       </div>
     );
