44658b9257919030febb457e79015f6e4302e012
lumenbarron <lucemendez@gmail.com>
2019-08-06 11:08:54 -0500

    making trivia

--- src/components/MemoryGame.js
+++ src/components/MemoryGame.js
@@ -2,17 +2,36 @@ import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import * as CARDIMG  from '../Data/IMAGES';
 import Nav from './NavBar';
+import a from '../ImagesTrivia/a-1.png';
 
 class MemoryGame extends Component {
   render() {
-    //CARDIMG.SINGSLETTERS.map((card)=>{console.log(card)});
     return (
       <div>
         <Nav />
-        <ShowCard />
-        {CARDIMG.SINGSLETTERS.map((card, index) => (
-          <ShowCard img={card} />
-        ))}
+        <section className="container-lsm">
+          <h5 className="title-lsm">Trivia</h5>
+
+          <section className="info-container">
+            <img className="image-sordo" src={a} />
+            <section className="info-ver-mas">
+              <p className="flow-text info-sordo">
+                ¿Qué letra representa la seña?
+              </p>
+
+              <select>
+                <option value="" disabled selected>
+                  Selecciona la opción correcta
+                </option>
+                <option value="1">A</option>
+                <option value="2">E</option>
+                <option value="3">J</option>
+              </select>
+            </section>
+                
+            <button className="waves-effect waves-light btn btn-ver-mas" >Siguiente</button>
+          </section>
+        </section>
       </div>
     );
   }
