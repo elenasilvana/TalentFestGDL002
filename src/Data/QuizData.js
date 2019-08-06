ef9acdb3726969d8028dee1ddad92057bdced4b5
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2019-08-06 13:17:11 -0500

    developing trivia functionality

---
+++ src/Data/QuizData.js
@@ -0,0 +1,31 @@
+export const QuizData = [
+    {
+        id: 0,
+        question: '¿Qué letra representa la seña?',
+        options: ['A', 'Y', 'E'],
+        answer: 'A'
+    },
+    {
+        id: 1,
+        question: '¿Qué letra representa la seña?',
+        options: ['Q', 'K', 'E'],
+        answer: 'K'
+    },
+    {
+        id: 2,
+        question: '¿Qué letra representa la seña?',
+        options: ['L', 'M', 'R'],
+        answer: 'L'
+    },
+    {
+        id: 3,
+        question: '¿Qué letra representa la seña?',
+        options: ['O', 'W', 'N'],
+        answer: 'N'
+    },  {
+        id: 4,
+        question: '¿Qué letra representa la seña?',
+        options: ['Z', 'Y', 'O'],
+        answer: 'Y'
+    }
+]
--- src/components/MemoryGame.js
+++ src/components/MemoryGame.js
@@ -1,33 +1,94 @@
 import React, { Component } from 'react';
 import ShowCard from './ShowCard';
-import * as CARDIMG  from '../Data/IMAGES';
+import * as CARDIMG from '../Data/IMAGES';
 import Nav from './NavBar';
-<<<<<<< HEAD
-import { timingSafeEqual } from 'crypto';
-=======
 import a from '../ImagesTrivia/a-1.png';
-import n from '../ImagesTrivia/n-1.png'
-import './cards.css'
->>>>>>> 5046f6f97c601792490cd533f501601117e7cab4
+import n from '../ImagesTrivia/n-1.png';
+import './cards.css';
+import {QuizData} from '../Data/QuizData';
 
 class MemoryGame extends Component {
-  constructor(props){
-    this.state={}
+    state = {
+      userAnswer: null,
+      currentQuestion: 0,
+      options: [],
+      quizEnd: false
+    }
+  
+  loadQuiz = () => {
+    const { currentQuestion } = this.state;
+    this.setState(() => {
+      return {
+        questions: QuizData[currentQuestion].question,
+        options: QuizData[currentQuestion].options,
+        answer: QuizData[currentQuestion].answer
+      }
+    })
   }
+  componentDidMount() {
+    this.loadQuiz();
+  }
+  nextQuestionHandler = () => {
+    this.setState({
+      currentQuestion: this.state.currentQuestion + 1
+    })
+    //console.log(this.state.currentQuestion);
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    const {currentQuestion} = this.state;
+    if(this.state.currentQuestion !== prevState.currentQuestion){
+      this.setState(()=>{
+        return {
+          questions: QuizData[currentQuestion].question,
+          options: QuizData[currentQuestion].options,
+          answer: QuizData[currentQuestion].answer
+        }
+      })
+    }
+  }
+  
+checkAnswer = (answer) => {
+  this.setState({userAnswer:answer})
+
+}
+
+finishHandler = () => {
+  if(this.state.currentQuestion === QuizData.length -1) {this.setState({quizEnd :true})}
+}
+
   render() {
-    //CARDIMG.SINGSLETTERS.map((card)=>{console.log(card)});
+    const { questions, options, currentQuestion, userAnswer, quizEnd } = this.state
+    if(quizEnd){
+      return(
+        <div>
+          <h3>Terminaste</h3>
+        </div>
+      )
+    }
     return (
       <div>
         <Nav />
-<<<<<<< HEAD
-        <ShowCard />
-        {CARDIMG.SINGSLETTERS.map((card, index) => (
-          <ShowCard img={card} />
-        ))}
-=======
         <section className="container-lsm">
           <h5 className="title-lsm">Trivia</h5>
+          <div>
+            <h4>{questions}</h4>
+            <span>{`pregunta ${currentQuestion} de ${QuizData.length - 1}`}</span>
+            {options.map(option=>(
+              <p className={`text-select ${userAnswer === option ? 'selected':null}`} onClick={ () => this.checkAnswer(option)} key={option.id} > 
+                {option}
+              </p>
 
+            ))}
+            {currentQuestion < QuizData.length - 1 && <button
+            onClick={this.nextQuestionHandler}>
+              siguiente
+
+            </button>}
+            
+            {currentQuestion === QuizData.length -1 && <button onClick={this.finishHandler}>Terminar
+            </button>}
+          </div>
           <section className="info-container">
             <img className="image-sordo" src={a} />
             <section className="info-ver-mas">
@@ -44,7 +105,7 @@ class MemoryGame extends Component {
                 <option value="3">J</option>
               </select>
             </section>
-                
+
             {/* <button className="waves-effect waves-light btn btn-siguiente" >Siguiente</button> */}
           </section>
 
@@ -64,12 +125,11 @@ class MemoryGame extends Component {
                 <option value="3">J</option>
               </select>
             </section>
-                
+
             {/* <button className="waves-effect waves-light btn btn-siguiente" >Siguiente</button> */}
           </section>
 
         </section>
->>>>>>> 5046f6f97c601792490cd533f501601117e7cab4
       </div>
     );
   }
